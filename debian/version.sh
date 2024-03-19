ee463b7846adb7ce5ea7c4ccf608841e601079b4
Single-beep <63972971+Single-beep@users.noreply.github.com>
2024-03-19 13:00:08 +0300

    GNU sed syntax

--- debian/version.sh
+++ debian/version.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-version=$(grep "version:" ../pubspec.yaml | cut -d ' ' -f2)
+version=$(grep "version:" pubspec.yaml | cut -d ' ' -f2)
 versionNumber=${version:0:5}
 prefix="const kPackageVersion ="
 version="${prefix} \"${versionNumber}\";"
-echo $version | tee ../lib/consts.dart
-echo $versionNumber | tee version
+echo $version | tee lib/consts.dart
+echo $versionNumber | tee debian/version
 
 debVersion="  Version: ${versionNumber}"
-file="debian.yaml"
-sed -i '' '9s/.*/'"${debVersion}"'/' $file
+file="debian/debian.yaml"
+sed -i '9s/.*/'"${debVersion}"'/' $file
